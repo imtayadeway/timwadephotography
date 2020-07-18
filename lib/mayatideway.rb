d1489641ef25b31fd4f6273e50d87dd5bdcb9d9f
Tim Wade <hello@timjwade.com>
2020-07-17 20:32:05 -0700

    Extract upload interface

--- lib/mayatideway.rb
+++ lib/mayatideway.rb
@@ -1,1 +1,12 @@
 require "mayatideway/photo"
+
+module Mayatideway
+  def self.upload(name:, path:, alt_text:, category:)
+    Photo.new(
+      name: name,
+      path: path,
+      alt_text: alt_text,
+      category: category
+    ).upload
+  end
+end
